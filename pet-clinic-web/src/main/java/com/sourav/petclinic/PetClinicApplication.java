fe7e5a17f1d37da9994f8c20b42fa3c1a1ca5449
sourav <souravsengupta366@gmail.com>
2020-08-08 10:14:24 +0530

    Created multiple modules

--- pet-clinic-web/src/main/java/com/sourav/petclinic/PetClinicApplication.java
+++ pet-clinic-web/src/main/java/com/sourav/petclinic/PetClinicApplication.java
@@ -1,4 +1,4 @@
-package com.sourav.petclinic.config;
+package com.sourav.petclinic;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
