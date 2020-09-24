abfa74abe2683c3f1cf679029d8ca69db71aec7e
sourav <souravsengupta366@gmail.com>
2020-09-24 10:08:11 +0530

    fixed the owner display

--- pet-clinic-web/src/main/java/com/sourav/petclinic/controller/OwnerController.java
+++ pet-clinic-web/src/main/java/com/sourav/petclinic/controller/OwnerController.java
@@ -4,7 +4,10 @@ import com.sourav.petclinic.model.Owner;
 import com.sourav.petclinic.services.OwnerServices;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 import java.util.Set;
 
@@ -18,11 +21,16 @@ public class OwnerController {
 
     @RequestMapping({"/owners","/owner","owners/index","owner/index","owners/index.html","owner/index.html"})
     public String ownerIndex(Model model){
-
-
         Set<Owner> setOfOwners = ownerServices.findAll();
         System.out.println("Size of owner's list = "+setOfOwners.size());
         model.addAttribute("owners",ownerServices.findAll());
         return "owners/index";
+    }
+
+    @GetMapping("/owners/{ownerId}")
+    public ModelAndView getOwner(@PathVariable Long ownerId){
+        ModelAndView mv = new ModelAndView("owners/ownerDetails");
+        mv.addObject(ownerServices.findById(ownerId));
+        return mv;
     }
 }
